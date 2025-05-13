b0f013ef73908f2476577024707a7de5cdec3cf7
Jose Maria De Lùcas Plata <d3lucasx@gmail.com>
2025-05-13 22:05:55 +0200

    Termino el ejemplo de programacion orientada a objetos

--- ProgramacionJava/src/POO/aparatosElectricos.java
+++ ProgramacionJava/src/POO/aparatosElectricos.java
@@ -2,42 +2,38 @@ package POO;
 
 public class aparatosElectricos {
 	
-	private int consumo;
+	private double consumo;
 	private boolean estado;
 	private static double consumoTotal = 0; // Ancla propiedad a la clase, para que sea comun para todos los objetos que crees, si no, cada objeto tendría un valor en este campo
-									 // y lo creamos para saber el consumo de todos los dispositivos.
-									 // STATIC par generar un campo/característica comun para todos los objetos que crees.
+									 		// y lo creamos para saber el consumo de todos los dispositivos.
+									 		// STATIC par generar un campo/característica comun para todos los objetos que crees.
 	
 	public aparatosElectricos(int consumo, boolean estado) {
 		
 		this.consumo = consumo;
 		this.estado= false;
 	}
-	
-	
-	public double calcularConsumo() {
-		if (estado == true) {
-			
-		}else if(estado == false){
-			this.consumo = 0;
-			consumoTotal -= this.consumo;
-		}
-	}
-	
+
 	public boolean isEstado() {
 		return estado;
 	}
-	
-	public void setEstado(boolean estado) {
-		if (estado == true) {
-			this.estado = true;
+
+	public double setEstado(boolean estado) {
+		if(estado == true) {
+			this.estado=true;
 			consumoTotal += consumo;
-		}else if (estado == false) {
+			
+		}else {
 			this.estado = false;
+			consumoTotal -= consumo;
+			if(consumoTotal < 0) {
+				consumoTotal = 0;
+			}
 		}
+		return consumoTotal;
 	}
 	
-	public static double consumoTotal() {
+	public static double printConsumoTotal() {
 		return consumoTotal;
 	}
 	
--- ProgramacionJava/src/POO/pruebaConsumo.java
+++ ProgramacionJava/src/POO/pruebaConsumo.java
@@ -4,12 +4,19 @@ public class pruebaConsumo {
 
 	public static void main(String[] args) {
 		
-		aparatosElectricos bombilla = new aparatosElectricos(0, 0, false);
-		aparatosElectricos radiador = new aparatosElectricos(0, 0, false);
-		aparatosElectricos plancha = new aparatosElectricos(0, 0, false);
+		aparatosElectricos bombilla = new aparatosElectricos(100, false);
+		aparatosElectricos radiador = new aparatosElectricos(2000, false);
+		aparatosElectricos plancha = new aparatosElectricos(1200, false);
 		
+		System.out.println(aparatosElectricos.printConsumoTotal());
+		System.out.println(bombilla.setEstado(true));
+		System.out.println(bombilla.setEstado(false));
+		System.out.println(radiador.setEstado(true));
+		System.out.println(radiador.setEstado(true));
+		System.out.println(bombilla.setEstado(false));
+		System.out.println(radiador.setEstado(false));
+		System.out.println(radiador.setEstado(false));
 		
-		System.out.println(aparatosElectricos.consumoTotal());
 	}
 	
 
